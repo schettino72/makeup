14d4903e736611c3d2fc0d17d09171415567b4b4
schettino72 <schettino72@gmail.com>
2013-01-14 02:35:20 +0800

    user real urls instead of hash

--- js/makeup.js
+++ js/makeup.js
@@ -3,7 +3,11 @@
  * MIT License
  */
 
+
 function Makeup (content_id, pages){
+    // @param content_id: (str) id of HTML element on template
+    // @param pages: (dict) page config info
+
     var sections = {};
     $.each(pages, function(k,v){
         sections[k] = new Section(v, content_id);
@@ -16,22 +20,51 @@ function Makeup (content_id, pages){
     }
 
     // bind events
-	$(window).bind('hashchange', $.proxy(this.hash_changed, this));
-    this.hash_changed();
+	//$(window).bind('hashchange', $.proxy(this.hash_changed, this));
+    //this.hash_changed();
+    $('body').on('click', 'a', $.proxy(this.handle_click, this));
+
+    this.set_page(location.pathname);
 }
 
-Makeup.PROCESSOR_MAP = {'md': function(data) { return markdown.toHTML(data);}};
-
-
-Makeup.prototype.hash_changed = function() {
-    var path = '/'; //default path
-	if ( location.hash ) {
-		path = location.hash.substring(1); // substring removes '#'
-	}
-    this.set_page(path);
+// processors that convert raw content into HTML
+Makeup.PROCESSOR_MAP = {
+    'md': function(data) { return markdown.toHTML(data);}
 };
 
+
+// based on jquery.pjax.js:handleClick
+// https://github.com/defunkt/jquery-pjax/blob/master/jquery.pjax.js
+Makeup.prototype.handle_click = function(event) {
+    var link = event.currentTarget;
+
+    // Middle click, cmd click, and ctrl click should open
+    // links in a new tab as normal.
+    if ( event.which > 1 || event.metaKey || event.ctrlKey ||
+         event.shiftKey || event.altKey )
+        return;
+
+    // Ignore cross origin links
+    if ( location.protocol !== link.protocol || location.host !== link.host )
+        return;
+
+    // Ignore anchors on the same page
+    if (link.hash && link.href.replace(link.hash, '') ===
+        location.href.replace(location.hash, ''))
+        return;
+
+    // Ignore empty anchor "foo.html#"
+    if (link.href === location.href + '#')
+        return;
+
+    this.set_page(link.pathname);
+
+    event.preventDefault();
+};
+
+
 Makeup.prototype.set_page = function(path){
+    // change location
     var page = this.pages[path];
     if (page === undefined){ // page not found
         // if no 404 (page not found) specified redirect to root
@@ -42,20 +75,21 @@ Makeup.prototype.set_page = function(path){
         // page not found - redirect to root
         page = this.pages['__404__'];
     }
-
     document.title = page.title;
     page.load();
 };
 
 
+
 function Section(opts, position){
+
     // convert shorthand single string to dict
     if (typeof opts === 'string'){
         opts = {src: opts};
     }
 
     this.title = opts.title; // (str) only used by pages
-    this.pos = opts.pos || position;
+    this.pos = opts.pos || position; // HTML id
     this.src = opts.src;
     this.processor = opts.processor;
     this.child = [];
@@ -76,20 +110,25 @@ function Section(opts, position){
     }
 }
 
-Section.prototype.load = function(pos, section){
+Section.prototype.load = function(){
     this.$ele = $('#' + this.pos);
     $.ajax({
         url: this.src,
         context: this,
-        success: this.load_cb
+        success: this.load_cb,
+        error: function(){console.log('oops');}
     });
 };
 
 Section.prototype.load_cb = function(data){
+    // load content callback
+    // 1 - process/convert raw data
     if (this.processor){
         data = this.processor(data);
     }
+    // 2 - insert into page
     this.$ele.html(data);
+    // 3 - load child elements
     $.each(this.child, function(_, child){
         child.load();
     });
